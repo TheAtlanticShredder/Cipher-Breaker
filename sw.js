587b725e47cf0893785fc935fc176920db4b4476
TheAtlanticShredder <adam.jewell@maine.edu>
2025-08-10 10:26:38 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="cipher-breaker-v1";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./sw.js","./icons/icon-192.png","./icons/icon-512.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener("activate",e=>{e.waitUntil(self.clients.claim());});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+const CACHE = "retro-runner-v1";
+const ASSETS = ["./","./index.html","./manifest.webmanifest","./sw.js","./icons/icon-192.png","./icons/icon-512.png"];
+self.addEventListener("install", e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener("activate", e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k)))) ); self.clients.claim(); });
+self.addEventListener("fetch", e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
